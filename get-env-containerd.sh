fa3b8c9fa6b80a5e0c5bb3ef6f2e0406b1fe3cb9
Ashwin Hendre <ashwin.hendre2@ibm.com>
2023-11-09 14:53:13 +0530

    Only test containerd v1.6.24 locally

--- get-env-containerd.sh
+++ get-env-containerd.sh
@@ -45,7 +45,8 @@ sed -i 's/Centos="latest"/Centos="centos7"/g' ${PATH_DOCKERTEST}/dockertest/vers
 # Get the docker-ce packages
 mkdir /workspace/docker-ce-${DOCKER_REF}_${DATE}
 cp -r ${PATH_COS}/s3_${COS_BUCKET_PRIVATE}/prow-docker/build-docker-${DOCKER_REF}_${DATE}/docker-ce-${DOCKER_REF}/* /workspace/docker-ce-${DOCKER_REF}_${DATE}
-
+mkdir /workspace/containerd-${CONTAINERD_REF}_${DATE}
+cp -r ${PATH_COS}/s3_${COS_BUCKET_PRIVATE}/prow-docker/build-docker-v24.0.7_301023-1025/containerd-${CONTAINERD_REF}/* /workspace/containerd-${CONTAINERD_REF}_${DATE}
 # Get the containerd packages if CONTAINERD_BUILD=0
 if [[ ${CONTAINERD_BUILD} = "0" ]]
 then
