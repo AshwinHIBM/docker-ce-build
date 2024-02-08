85c92507af0d1f35f2c87e736d0cfdd760b507c9
Ashwin Hendre <ashwin.hendre2@ibm.com>
2024-02-08 15:30:06 +0530

    Dont build containerd only run tests

--- get-env-containerd.sh
+++ get-env-containerd.sh
@@ -45,7 +45,8 @@ sed -i 's/Centos="latest"/Centos="centos7"/g' ${PATH_DOCKERTEST}/dockertest/vers
 # Get the docker-ce packages
 mkdir /workspace/docker-ce-${DOCKER_TAG}_${DATE}
 cp -r ${PATH_COS}/s3_${COS_BUCKET_PRIVATE}/prow-docker/build-docker-${DOCKER_TAG}_${DATE}/docker-ce-${DOCKER_TAG}/* /workspace/docker-ce-${DOCKER_TAG}_${DATE}
-
+ mkdir /workspace/containerd-${CONTAINERD_TAG}_${DATE}
+cp -r ${PATH_COS}/s3_${COS_BUCKET_PRIVATE}/prow-docker/build-docker-v25.0.3_080224-0717/containerd-${CONTAINERD_TAG}/* /workspace/containerd-${CONTAINERD_TAG}_${DATE}
 # Get the containerd packages if CONTAINERD_BUILD=0
 if [[ ${CONTAINERD_BUILD} = "0" ]]
 then
