9b763a7ca74cf3fd97250dde181fd402931cee89
Ashwin Hendre <ashwin.hendre2@ibm.com>
2024-04-08 17:49:02 +0530

    Bypass test patch

--- test.sh
+++ test.sh
@@ -431,7 +431,7 @@ echo "# Tests of the dynamic packages #"
 before=$SECONDS
 # 1) Build the list of distros
 # List of Distros that appear in the list though they are EOL or must not be built
-DisNo+=( "ubuntu-impish" "debian-buster" )
+DisNo+=( "ubuntu-impish" "debian-buster" "centos-7" "fedora-40" )
 for PACKTYPE in DEBS RPMS
 do
   for DISTRO in ${!PACKTYPE}
