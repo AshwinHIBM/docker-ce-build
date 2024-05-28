d2d761fc19f709da190a7b17ce5517e9367d7ae1
Ashwin Hendre <ashwin.hendre2@ibm.com>
2024-05-28 14:11:34 +0530

    Use custom dockerd

--- upstream-master-ci/get-env-ci.sh
+++ upstream-master-ci/get-env-ci.sh
@@ -1,7 +1,3 @@
 set -o allexport
 # Clone the moby repository
-git clone https://github.com/moby/moby.git
-sed -i 's@export TEST_CLIENT_BINARY=/usr/local/cli/$(basename "$DOCKER_CLI_PATH")@TEST_CLIENT_BINARY=/usr/local/bin/dockerd@g' moby/hack/make/.integration-daemon-start
-sed -i '73i wget -o /usr/local/bin/dockerd-entrypoint.sh https://raw.githubusercontent.com/docker-library/docker/master/26/dind/dockerd-entrypoint.sh' moby/hack/make/.integration-daemon-start
-sed -i '74i git clone https://github.com/ppc64le-cloud/docker-ce-build.git' moby/hack/make/.integration-daemon-start
-sed -i '75i pushd docker-ce-build && dockerctl.sh start && popd' moby/hack/make/.integration-daemon-start
+git clone https://github.com/AshwinHIBM/moby.git
