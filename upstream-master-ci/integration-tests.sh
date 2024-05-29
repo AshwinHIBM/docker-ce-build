7b412d6a536339b00ed10a92ff244d191d51cd96
Ashwin Hendre <ashwin.hendre2@ibm.com>
2024-05-29 18:48:57 +0530

    Test skip integration CLI flag

--- upstream-master-ci/integration-tests.sh
+++ upstream-master-ci/integration-tests.sh
@@ -1,7 +1,7 @@
 pushd moby
 mkdir tmp
 touch tmp/out.txt
-make -o build test-integration 2>&1 | tee tmp/out.txt
+make -o TEST_SKIP_INTEGRATION_CLI=true build test-integration 2>&1 | tee tmp/out.txt
 rc=$(grep "failure" tmp/out.txt | awk '{print $6;}')
 popd
 if [[ $rc == 0 ]]; then
