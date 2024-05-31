4a8b5e22b0b960ad19e42a06bba401abbcecc2da
Ashwin Hendre <ashwin.hendre2@ibm.com>
2024-05-31 15:12:54 +0530

    Store CI logs

--- upstream-master-ci/integration-tests.sh
+++ upstream-master-ci/integration-tests.sh
@@ -1,8 +1,30 @@
 pushd moby
-mkdir tmp
-touch tmp/out.txt
-TEST_IGNORE_CGROUP_CHECK=true TESTDEBUG="true" make -o build test-integration 2>&1 | tee tmp/out.txt
-rc=$(grep "failure" tmp/out.txt | awk '{print $6;}')
+
+# Store logs by date to avoid overwriting
+DATE=`date +%d%m%y-%H%M`
+export DATE=${DATE}
+
+checkDirectory() {
+  if ! test -d $1
+  then
+    mkdir $1
+    if [[ $? -ne 0 ]]; then
+      exit 1
+    fi
+    echo "$1 created"
+  else
+    echo "$1 already created"
+  fi
+}
+
+DIR_LOGS_COS="/mnt/s3_ppc64le-docker/prow-docker/ppc64le-ci/${DATE}"
+checkDirectory ${DIR_LOGS_COS}
+
+set -x
+TEST_IGNORE_CGROUP_CHECK=true TESTDEBUG="true" make -o build test-integration 2>&1 | tee ${DIR_LOGS_COS}/integration.log
+set +x
+
+rc=$(grep "failure" ${DIR_LOGS_COS}/integration.log | awk '{print $6;}')
 popd
 if [[ $rc == 0 ]]; then
   exit 0
--- upstream-master-ci/unit-tests.sh
+++ upstream-master-ci/unit-tests.sh
@@ -1,8 +1,27 @@
 pushd moby
-mkdir tmp
-touch tmp/out.txt
-make -o build test-unit 2>&1 | tee tmp/out.txt
-rc=$(grep "failure" tmp/out.txt | awk '{print $6;}')
+
+# Store logs by date to avoid overwriting
+DATE=`date +%d%m%y-%H%M`
+export DATE=${DATE}
+
+checkDirectory() {
+  if ! test -d $1
+  then
+    mkdir $1
+    if [[ $? -ne 0 ]]; then
+      exit 1
+    fi
+    echo "$1 created"
+  else
+    echo "$1 already created"
+  fi
+}
+
+DIR_LOGS_COS="/mnt/s3_ppc64le-docker/prow-docker/ppc64le-ci/${DATE}"
+checkDirectory ${DIR_LOGS_COS}
+
+make -o build test-unit 2>&1 | tee ${DIR_LOGS_COS}/unit.log
+rc=$(grep "failure" ${DIR_LOGS_COS}/unit.log | awk '{print $6;}')
 cp bundles/junit-report.xml ${ARTIFACTS}
 popd
 
