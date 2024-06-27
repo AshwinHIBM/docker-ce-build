aea9d059c8ee3a19dd6fa89fd6d8a02b2b749a13
Ashwin Hendre <ashwin.hendre2@ibm.com>
2024-06-27 16:45:42 +0530

    Use my fork of CLI

--- upstream-master-ci/get-env-ci.sh
+++ upstream-master-ci/get-env-ci.sh
@@ -1,4 +1,4 @@
 set -o allexport
 # Clone the moby repository
 git clone https://github.com/moby/moby.git
-git clone https://github.com/docker/cli.git
+git clone https://github.com/AshwinHIBM/cli.git
