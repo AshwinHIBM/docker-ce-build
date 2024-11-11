7df66ff8b83fccb0da4657a8bb54a331d34b5b72
Ashwin Hendre <ashwin.hendre2@ibm.com>
2024-11-11 12:10:44 +0530

    Build containerd 1.7.22 for Fedora-41

--- build-containerd.sh
+++ build-containerd.sh
@@ -154,9 +154,9 @@ before=$SECONDS
 # 1) Build the list of distros
 # List of Distros that appear in the list though they are EOL or must not be built
 DisNo+=( "ubuntu-impish" "debian-buster" )
-for PACKTYPE in DEBS
+for PACKTYPE in RPMS
 do
-  for DISTRO in "ubuntu-oracular"
+  for DISTRO in "fedora-41"
   do
     No=0
     for (( d=0 ; d<${#DisNo[@]} ; d++ ))
