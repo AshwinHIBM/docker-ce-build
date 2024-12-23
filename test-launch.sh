45c318c567f1f74db417fd42151838a0ce2e4d1d
Ashwin Hendre <ashwin.hendre2@ibm.com>
2024-12-23 16:57:14 +0530

    Skip runc version test

--- test-launch.sh
+++ test-launch.sh
@@ -17,11 +17,11 @@ echo ""
 DOCKER_CLI_VERSION=$(docker version --format '{{.Client.Version}}')
 DOCKER_SERVER_VERSION=$(docker version --format '{{.Server.Version}}')
 CONTAINERD_VERSION=$(docker version| awk '/containerd/{getline; print $2}')
-RUNC_VERSION=$(docker version| awk '/runc/{getline; print $2}')
-if [[ ${RUNC_VERSION} != ${CONTAINERD_RUNC_TAG:1} ]]; then
-  echo "ERROR: Version mismatch: RUNC version being tested is ${CONTAINERD_RUNC_TAG:1} and RUNC version downloaded from the Docker website is ${RUNC_VERSION}"
-  exit 1
-fi
+# RUNC_VERSION=$(docker version| awk '/runc/{getline; print $2}')
+# if [[ ${RUNC_VERSION} != ${CONTAINERD_RUNC_TAG:1} ]]; then
+#   echo "ERROR: Version mismatch: RUNC version being tested is ${CONTAINERD_RUNC_TAG:1} and RUNC version downloaded from the Docker website is ${RUNC_VERSION}"
+#   exit 1
+# fi
 
 if [[ ${DOCKER_CLI_VERSION:1} != ${DOCKER_TAG:1} ]]; then
   echo "ERROR: Version mismatch: Docker CLI version being tested is ${DOCKER_TAG:1} and Docker CLI version downloaded from the Docker website is ${DOCKER_CLI_VERSION:1}"
